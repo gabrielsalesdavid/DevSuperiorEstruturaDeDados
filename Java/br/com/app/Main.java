d85ff63e68120db9f3182f4989401a3cb293accf
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-30 09:44:40 -0300

    Structure in Java

---
+++ Java/br/com/app/Main.java
@@ -0,0 +1,15 @@
+package br.com.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Product p01 = new Product("Laptop", 1000.00, 5);
+        Product p02 = new Product("HeadPhones", 200.00, 2);
+
+        System.out.println(p01);
+        System.out.println(p02);
+        System.out.println(p01.name);
+        System.out.println(p02.name);
+    }
+}
---
+++ Java/br/com/app/Product.java
@@ -0,0 +1,25 @@
+package br.com.app;
+
+public class Product {
+
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+
+        return name
+        + ", $"
+        + String.format("%.f2", price)
+        + ", "
+        + quantity;        
+    }
+}
