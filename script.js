b6218750e9acf393c381bb8d47635b494ccfd540
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-30 08:20:40 -0300

    Init JS and HTML. Type structur

---
+++ script.js
@@ -0,0 +1,23 @@
+class Product {
+
+    constructor(name, price, quantity) {
+
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    toString() {
+
+        return this.name + ", " + this.price;
+    }
+}
+
+const p01 = new Product("Laptop", 1000.00, 5);
+const p02 = new Product("HeadPhones", 200.00, 2)
+
+console.log(p01.name)
+console.log(p01.price)
+
+console.log(p02.name)
+console.log(p02.price)
