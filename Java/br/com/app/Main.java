0747a5b18927db53ea145421c0c63990abc37fdd
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-30 10:38:15 -0300

    non-object-oriented function

--- Java/br/com/app/Main.java
+++ Java/br/com/app/Main.java
@@ -2,6 +2,16 @@ package br.com.app;
 
 public class Main {
 
+    static double total(Product product) {
+
+        return product.getPrice() * product.getQuantity();
+    }
+
+    static void updatePrice(Product product, double percentage) {
+
+        double newPrice = product.getPrice() * (1 + percentage / 100.0);
+    }
+
     public static void main(String[] args) {
 
         Product p01 = new Product("Laptop", 1000.00, 5);
@@ -12,5 +22,14 @@ public class Main {
 
         System.out.println(p01.getPrice());
         System.out.println(p02.getPrice());
+
+        double total = total(p01);
+        double total01 = total(p02);
+
+        System.out.println(total);
+        System.out.println(total01);
+
+        updatePrice(p01, 20.0);
+        System.out.println(p01.getPrice());
     }
 }
