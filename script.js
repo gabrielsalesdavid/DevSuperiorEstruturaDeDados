a49499a0391602836b32c64c68e7d3211d1bb405
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-30 09:04:51 -0300

    Function and const

--- script.js
+++ script.js
@@ -19,6 +19,16 @@ class Product {
     }*/
 }
 
+function total(product) {
+
+    return product.price * product.quantity;
+}
+
+function updatePrice(product, percentage) {
+
+    product.price = product.price * (1 + percentage / 100);
+}
+
 const p01 = new Product("Laptop", 1000.00, 5);
 const p02 = new Product("HeadPhones", 200.00, 2)
 
@@ -30,3 +40,13 @@ console.log(p02.price)
 
 console.log(p02.toString())
 console.log(p02.toString())
+
+
+const total01 = total(p01);
+const total02 = total(p02);
+
+console.log(total01);
+console.log(total02);
+
+updatePrice(p01, 10);
+console.log(p01.price);
