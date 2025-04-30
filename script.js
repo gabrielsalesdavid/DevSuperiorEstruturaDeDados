07c911fe3a3eff0f81c7705384b7603e0d2c9f79
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-30 08:39:16 -0300

    alter

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ class Product {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-    }
 
+        this.toString = function() {
+
+            return `${this.name}, $${this.price.toFixed(2)}, ${this.quantity}`;
+        }
+    }
+/*
     toString() {
 
         return `${this.name}, $${this.price.toFixed(2)}, ${this.quantity}`;
         //this.name + ", " + this.price;
-    }
+    }*/
 }
 
 const p01 = new Product("Laptop", 1000.00, 5);
