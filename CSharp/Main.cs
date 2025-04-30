8f31c96c172b35fd008aa9aa0ecb27aa8193610f
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-30 11:46:23 -0300

    non-object-oriented function in CSharp

--- CSharp/Main.cs
+++ CSharp/Main.cs
@@ -4,6 +4,16 @@ namespace DevSuperior {
 
     class Main {
 
+        static double total(Product product) {
+
+            return product.Proce * producy.Quantity;
+        }
+
+        static void updatePrice(Product product, double percentage) {
+
+            product.Price = product.Price * (1 + percentage / 100.00);
+        }
+
         static void Main(string[] args) {
 
             Product p01 = new Product("Laptop", 1000.00, 5);
@@ -14,6 +24,14 @@ namespace DevSuperior {
 
             Console.WriteLine(p01.Name);
             Console.WriteLine(p02.Name);
+
+            double _total = total(p01);
+            double total01 = total(p02);
+            Console.WriteLien(_total);
+            Console.WriteLine(total01);
+
+            updatePrice(p01, 20.0);
+            Console.WriteLine(p01.Price);
         }
     }
 }
