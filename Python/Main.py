8628e5d19216a4ab480e0548ee523a4d4439b1c1
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-04-30 12:23:55 -0300

    Function in Python non-object-oriented

--- Python/Main.py
+++ Python/Main.py
@@ -8,6 +8,18 @@ class Product:
     def __str__(self):
         return f"{self.name}, $ {self.price:.2f}, {self.quantity}"
         
+
+
+    def total(product):
+
+        return product.price * product.quantity
+    
+
+    def update_price(product, percentage):
+
+        product.price = product.price * (1 + percentage / 100.00)
+
+
 p01 = Product("LapTop", 1000.00, 5)
 p02 = Product("HeadPhone", 200.00, 22)
 
@@ -15,3 +27,14 @@ print(p01)
 print(p02)
 print(p01.name)
 print(p02.name)
+
+total01 = total(p01)
+total02 = total(p02)
+
+print(total01)
+print(total02)
+
+update_price(p01, 10)
+update_price(p02, 20)
+print(p01.Price)
+print(p02.Price)
