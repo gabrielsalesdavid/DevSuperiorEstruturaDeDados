1a085a7f3b43e55939b3dc39d872efb167131289
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-05-06 13:44:55 -0300

    Reading JSON file and processing timestamp type field (Part 2)

---
+++ Web01/HTML/JSON/read-file.py
@@ -0,0 +1,16 @@
+import json
+from dateutil import parser
+
+with open('JSon/file.json', 'r') as file:
+    jsonData = json.load(file)
+
+def parse_timestamp(timesptamp):
+    return parser.isoparse(timestamp)
+
+for course in jsonData:
+    print(f"Curso: {course['title']}")
+
+    for lesson in course['lessons']:
+        print(f"Aula {lesson['id']}: {lesson['title']}")
+        print(f"Media: {lesson['media']}")
+        print(f"Timestamp: {parse_timestamp(lesson['timestamp'])}")
