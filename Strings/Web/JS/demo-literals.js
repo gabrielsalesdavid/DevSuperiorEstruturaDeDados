c688f0c123bdb20fd99c2ca035a3fb4c1c59b198
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-05-12 23:08:07 -0300

    Literals and expressions in Javascript PART 1

---
+++ Strings/Web/JS/demo-literals.js
@@ -0,0 +1,14 @@
+const name = "Alice";
+const age = 31;
+const points = 8.347;
+
+const str01 = "Olá, mundo!";
+const str02 = 'Olá, mundo!';
+const str03 = `Olá, mundo!`;
+
+console.log("str01:");
+console.log(str01);
+
+const str04 = "Cliente " + name + " tem idade " + age + " anos e fez " + points + " pontos.";
+console.log("str04:");
+console.log(str04);
