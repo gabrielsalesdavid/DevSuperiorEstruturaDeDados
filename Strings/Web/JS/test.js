d688f3713234035ef93439d116483a7d28a6a363
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-05-18 15:33:15 -0300

    Example of finding emails in a text

--- Strings/Web/JS/test.js
+++ Strings/Web/JS/test.js
@@ -84,3 +84,32 @@ function validateBrDomain(domain) {
 
 console.log(validateBrDomain("batata.com.br"));
 console.log(validateBrDomain("banana.com"));
+
+function findEmails(string) {
+
+    const regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/g;
+    const result = string.match(regex);
+    if(result) {
+
+        return result;
+    }
+
+    return [];
+}
+
+const text = "Para mais informações, contate-nos em contato@exemplo.com ou suporte@exemplo.com.br.";
+
+const emails = findEmails(text);
+
+if(emails.length > 0) {
+
+    console.log("Emails encontrados: ");
+
+    for(let i = 0; i < emails.length; i++) {
+
+        console.log(emails[i]);
+    }
+} else {
+
+    console.log("Nenhum email encontrado.");
+}
