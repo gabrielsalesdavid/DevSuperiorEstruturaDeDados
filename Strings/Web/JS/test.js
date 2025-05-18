3e13c6e22549a7d335a051570ed11c9cb69c1463
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-05-18 13:22:11 -0300

    String Functions in Javascript PART 2

--- Strings/Web/JS/test.js
+++ Strings/Web/JS/test.js
@@ -24,3 +24,23 @@ console.log("str03.match(/test/g):", str03.match(/test/g));
 
 let matchAllResult02 = Array.from(str03.matchAll("test"));
 console.log("Array.from(str03.matchAll('test')):", matchAllResult02);
+
+console.log("str03.replace('test', 'demo'):", str03.replace("test", "demo"));
+
+console.log("str03.replaceAll('test', 'demo'):", str03.replaceAll("test", "demo"));
+
+console.log("str03.search('bla vla'):", str03.search("blabla"));
+
+console.log("str03.split(' '):", str03.split(" "));
+
+const s = "batata tomate abacaxi";
+const vect = s.split(' ');
+console.log(s[1]);
+
+console.log("str04.slice(-3):", `"${str04.slice(-3)}"`);
+console.log("str04.slice(2, 5):", `"${str04.slice(2, 5)}"`);
+console.log("str04.slice(5, 2):", `"${str04.slice(5, 2)}"`);
+
+console.log("str04.substring(-3):", `"${str04.substring(-3)}"`);
+console.log("str04.substring(2, 5):", `"${str04.substring(2, 5)}"`);
+console.log("str04.substring(5, 2):", `"${str04.substring(5, 2)}"`);
