ba699113be0db90f3340dbe9e12e6310b9ba5dd0
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-05-20 22:43:29 -0300

    Anagram problem workaround using array

--- Exercicio/JSanagram/test.js
+++ Exercicio/JSanagram/test.js
@@ -18,6 +18,33 @@ function isAnagram(s, t) {
     }
 }
 
+function isAnagram01(s, t) {
+
+    const count = new Array(26).fill(0);
+
+    for(let i = 0; i < s.length; i++) {
+
+        const index = s.charCodeAt(i) - 'a'.charCodeAt(0);
+        count[index]++;
+    }
+
+    for(let i = 0; i < t.length; i++) {
+
+        const index = t.charCodeAt(i) - 'a'.charCodeAt(0);
+        count[index]--;
+    }
+
+    for(let val of count) {
+
+        if(val !== 0) {
+
+            return false;
+        }
+    }
+    
+    return true;
+}
+
 console.log(isAnagram("anagram", "naragam"));
 console.log(isAnagram("rat", "car"));
 console.log(isAnagram("batata", "attaba"));
