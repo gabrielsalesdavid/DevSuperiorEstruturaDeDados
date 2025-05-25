a8f9b6f8cc50b3d542794323224b35e0ed33760a
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2025-05-25 08:43:46 -0300

    Transactions Problem Solution PART 2

--- Exercicio/JStransaction/test.js
+++ Exercicio/JStransaction/test.js
@@ -1,4 +1,39 @@
-const transactions = ["alice,20,800,mtv","alice,50,1200,mtv"];
+function invalidTransaction(transactions) {
+
+    const result = [];
+    const invalidArray = new Array(transactions.length).fill(false);
+
+    for(let i = 0; i < transactions.length; i++) {
+
+        const ti = new Transaction(transactions[i]);
+        
+        if(ti.money > 1000) {
+
+            invalidArray[i] = true;            
+        }
+
+        for(let j = i; j < transactions.length; j++) {
+
+            const tj = Transaction(transactions[j]);
+
+            if(ti.name === tj.name && Math.abs(ti.tim - tj.time) <= 60 && ti.city !== tj.city) {
+
+                invalidArray[i] = true;
+                invalidArray[j] = true;
+            }
+        }
+    }
+
+    for(let i = 0; i < transactions.length; i++) {
+
+        if(invalidArray[i]) {
+
+            result.push(transactions[i]);
+        }
+    }
+
+    return result;
+}
 
 function Transaction(csv) {
 
@@ -8,6 +43,10 @@ function Transaction(csv) {
     this.name = parts[2];
     this.name = parts[3];
 }
+
+invalidTransaction(["alice,20,800,mtv","alice,50,1200,mtv"]);
+
+const transactions = ["alice,20,800,mtv","alice,50,1200,mtv"];
 
 const t01 = new Transaction(transactions[0]);
 const t02 = new Transaction(transactions[1]);
