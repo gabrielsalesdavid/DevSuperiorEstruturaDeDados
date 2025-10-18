68c39261e336e58cd1b5f580894167ac831ebe36
Gabriel Sales David <51879605+gabrielsalesdavid@users.noreply.github.com>
2025-10-18 11:46:58 -0300

    Time Teste

---
+++ Exercicio/time-test.js
@@ -0,0 +1,16 @@
+function myFunction() {
+
+    let x = 10;
+    for (let i = 0; i < 100000000; i ++) {
+
+        x = - x;
+    }
+};
+
+let star = Date.now();
+myFunction();
+let end = Date.now();
+
+let time = end - start;
+
+console.log(`Duration: ${time}ms`);
